4d2fa935b4c8139f32aecff4d23ac475d0e4bfe7
Marcel Marques <marcmatiasdev@gmail.com>
2023-03-13 11:31:42 -0300

    Deleting old files

--- example/demo-purposes-toggle-theme.js
+++
@@ -1,5 +0,0 @@
-// Toggle if user OS theme setted as dark mode [demo purposes only]
-if (window.matchMedia("(prefers-color-scheme: dark)").matches) toggleTheme();
-function toggleTheme() {
-  document.querySelector("html").classList.toggle("dark");
-}
--- src/index.js
+++
@@ -1,7 +0,0 @@
-import "./assets/css/select-actions.css";
-import SelectActions from "./select-actions";
-import { createOptions } from "./utils";
-
-SelectActions.createOptions = createOptions;
-
-export default SelectActions;
